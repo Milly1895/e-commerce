8eeab5ae103c1b9bd84a9d391198e58240ae4339
jamily silva <jamilysilva3614@gmail.com>
2026-07-06 16:34:51 -0300

    feat(produto):Remove a importação do RouterOutlet e Adicionar componente produto no arquivo app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+//!import { RouterOutlet } from '@angular/router';//Remove importação do RouterOutlet,pois não é necessário para o componente App
+import { Produto } from './components/produto/produto';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
