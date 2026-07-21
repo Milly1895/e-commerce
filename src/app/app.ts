e1a064477fc38ba86b97efb6ba907f4f78a84ac8
jamily silva <jamilysilva3614@gmail.com>
2026-07-21 14:28:28 -0300

    adiciona variável para exibir o nome da loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-//!import { RouterOutlet } from '@angular/router';//Remove importação do RouterOutlet,pois não é necessário para o componente App
-import { Produto } from './features/produtos/produto/produto';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+//import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'JS silva'; //nome do e-commerce
 }
