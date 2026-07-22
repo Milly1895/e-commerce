4994e762a782b7d59f6f73f31f960edfa2d067e3
jamily silva <jamilysilva3614@gmail.com>
2026-07-22 16:39:41 -0300

    chore:adiciona import do usuarioLogado e import de UpperCasePipe

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { Produto} from './features/produtos/produto/produto';
+import { usuarioLogado,login,logout } from './core/auth';
+import { UpperCasePipe } from '@angular/common';
 //import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink,UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
   nomeLoja = 'JS silva'; //nome do e-commerce
+  usuarioLogado = usuarioLogado;
+  login = login;
+  logout = logout;
 }
