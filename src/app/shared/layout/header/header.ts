8ed973a70b3c7e42f9e322badd3f52b500b22577
jamily silva <jamilysilva3614@gmail.com>
2026-08-04 17:14:56 -0300

    chore: cria pasta shared layoutheader

---
+++ src/app/shared/layout/header/header.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatButtonModule, MatToolbarModule,MatIconModule,RouterLink, UpperCasePipe],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  nomeLoja = 'Pewiccon'
+}
