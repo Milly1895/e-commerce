9324112c4f754313b3c3f06337ce8a76f46653e6
jamily silva <jamilysilva3614@gmail.com>
2026-08-19 17:25:02 -0300

    feat: adicionar nova funcionalidade

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,33 +1,48 @@
-import { inject, Injectable } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { inject, Injectable, PLATFORM_ID } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
 import { EmailValidator } from "@angular/forms";
+
 
 type PerfilUsuario = 'admin ' | 'usuario';
 
 type Usuario = {
     email: string;
     perfil:  PerfilUsuario;
-}
+};
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
-    private usuario =signal <Usuario | null >(null);
-    private tokenJwt = signal <string |null>(null);
+    private usuario =signal <Usuario | null >(this.carregarUsuarioSalvo());
+    private tokenJwt = signal <string |null>(this.carregarTokenSalvo());
 
     usuarioAtual = computed(() => this.usuario());
     token = computed(() => this.tokenJwt());
-    usuarioLogado = computed(() => this.usuario() !==null);
     admin = computed(() => this.usuario()?.perfil === 'admin ');
+
+    //! =========== PERSISTÊNCIA AUTH ============
+
+    private platformId = inject(PLATFORM_ID);
+    private readonly chaveUsuario = 'usuario-storage';
+    private readonly chaveToken = 'token-storage';
+    
+    usuarioLogado = computed(() => this.usuario() !== null && this.tokenJwt() !== null);
 
     login(email: string, senha: string): boolean{
         if(!email || !senha){
             return false;
         }
         const perfil:PerfilUsuario = email === 'admin@email.com' ? 'admin ' : 'usuario';
+
+        const usuarioLogado: Usuario = {
+            email,
+            perfil,
+        };
+
         const tokenSimulado = 
 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
 'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -35,17 +50,63 @@ export class AuthService {
 
 this.usuario.set({
     email,
-    perfil
+    perfil,
 });
 
 this.tokenJwt.set(tokenSimulado);
+this.usuario.set(usuarioLogado);
+this.salvarAutenticacao(usuarioLogado, tokenSimulado);
+
 return true;
     }
     logout(){
         this.usuario.set(null);
         this.tokenJwt.set(null);
+        this.limparAutenticacaoSalva();
     }
     obterToken(): string | null{
         return this.tokenJwt();
     }
+    obterPerfil():PerfilUsuario | null {
+        return this.usuario()?.perfil ?? null;
+    }
+    private estaNoNavegador(): boolean{
+        return isPlatformBrowser(this.platformId);
+    }
+    private carregarUsuarioSalvo(): Usuario | null {
+        if(!this.estaNoNavegador()){
+            return null;
+        }
+        const dadosSalvos = localStorage.getItem(this.chaveUsuario);
+        if(!dadosSalvos){
+            return null;
+        } try {
+            return JSON.parse(dadosSalvos) as Usuario;
+        } catch{
+            return null;
+        }
+     }
+     private carregarTokenSalvo(): string | null {
+        if(!this.estaNoNavegador()){
+            return null;
+        }
+        return localStorage.getItem(this.chaveToken);
+     }
+     private salvarAutenticacao(usuario: Usuario, token: string){
+
+        if(!this.estaNoNavegador()){
+            return;
+        }
+
+        localStorage.setItem(this.chaveUsuario,JSON.stringify(usuario));
+        localStorage.setItem(this.chaveToken, JSON.stringify(token));
+     }
+
+     private limparAutenticacaoSalva(){
+        if(!this.estaNoNavegador()){
+            return;
+        }
+        localStorage.removeItem(this.chaveUsuario);
+        localStorage.removeItem(this.chaveToken);
+     }
 }
--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,36 +1,78 @@
-import { Injectable,signal } from "@angular/core";
+import { effect, inject, Injectable,signal } from "@angular/core";
 import { Signal } from "@angular/core";
 import { computed } from "@angular/core";
 import { single } from "rxjs";
 import { ListaProdutos } from "../../features/produtos/lista-produtos/lista-produtos";
+import { ItemCarrinho } from "../models/item-carrinho";
+import { isPlatformBrowser } from "@angular/common";
+import { PLATFORM_ID } from "@angular/core";
+import { json } from "stream/consumers";
 
-
-type ItemCarrinho = {
-    nome: string;
-    preco:number;
-}
 @Injectable({
     providedIn: 'root'
 })
 
 export class CarrinhoService{
+
 //! Estado Global
-private carrinho = signal<ItemCarrinho[]>([]);
+private carrinho = signal<ItemCarrinho[]>(this.carregarcarrinhSalvo());
 //? Seletores
 itens = computed(()=> this.carrinho());
-quantidadesItens = computed(() => this.carrinho().length);
+quantidadesItens = computed(() => this.carrinho().length);  //! Quantidade de itens do carrinho
 totalItens = computed(()=>
     this.carrinho().reduce((total,item)=>total=item.preco,0)
 );
 
 carrinhoVazio = computed(() => this.carrinho().length === 0);
+  //! ========= PERSISTÊNCIA CARRINHO =========
+   private platformId = inject(PLATFORM_ID);
+
+   //! Chave de recuperção Localstorage
+   private readonly chaveStorage = 'carrinho-storage';
+
+constructor(){
+    effect(() =>{
+        this.salvarCarrinho(this.carrinho());
+    });
+}
+
+private estaNoNavegador(): boolean{
+    return isPlatformBrowser(this.platformId);
+}
+
+private carregarcarrinhSalvo(): ItemCarrinho [] {
+    if(!this.estaNoNavegador()){
+        return[];
+    }
+    const dadosSalvos = localStorage.getItem(this.chaveStorage);
+    if(!dadosSalvos){
+        return[];
+    }
+
+    try {
+        return JSON.parse(dadosSalvos) as ItemCarrinho[];
+        
+    }catch{
+        return[];
+    }
+}
+private salvarCarrinho(item: ItemCarrinho[]){
+    if(!this.estaNoNavegador()){
+        return;
+    }
+    localStorage.setItem(this.chaveStorage, JSON.stringify(item));
+}
+
 //Todo: Ações
 
 adicionar(produto:ItemCarrinho){
-    this.carrinho.update(lista =>[...lista,produto]);
+    this.carrinho.update(lista =>[...lista,produto])
 }
 limpar(){
     this.carrinho.set([]);
 }
-
+removerItem(rmvItem:number){
+    this.carrinho.update((listaAtual) =>
+    listaAtual.filter((_, index) => index !== rmvItem));
+}
 }
