cb5e0b46be01d5083a2198c4deed662bb9252693
jamily silva <jamilysilva3614@gmail.com>
2026-08-19 17:00:14 -0300

    chore: atualizção

---
+++ src/app/core/facades/carrinho.facade.ts
@@ -0,0 +1,26 @@
+import { Injectable,inject } from "@angular/core";
+import { CarrinhoService } from "../services/carrinho.service";
+import { ItemCarrinho } from "../models/item-carrinho";
+
+@Injectable({providedIn: 'root'})
+
+export class CarrinhoFacade {
+
+    private carrinhoService = inject(CarrinhoService);
+
+    itensCarrinho = this.carrinhoService.itens;
+    quantidadeCarrinho = this.carrinhoService.quantidadesItens;
+    totalCarrinho = this.carrinhoService.totalItens;
+    carrinhoVazio = this.carrinhoService.carrinhoVazio;
+
+    adicionarProdutoCarrinho(produto: ItemCarrinho){
+        this.carrinhoService.adicionar(produto);
+    }
+
+    limparCarrinho(){
+        this.carrinhoService.limpar();
+    }
+    removerItem(rmvItem:number){
+        this.carrinhoService.removerItem(rmvItem);
+    }
+}
